02b116d9913db51483f3a62ae162b5dbcf69da9e
cwxyz007 <jie844067636@gmail.com>
2018-11-14 23:02:53 +0800

    feat: Add comment form

--- src/comment.js
+++ src/comment.js
@@ -1,7 +1,29 @@
 import firebase from 'firebase';
+import { login } from './login';
 
 function init(config) {
   firebase.initializeApp(config);
 }
+
+document.getElementById('login').onclick = () => {
+  login().then((data) => {
+    console.log(data);
+  });
+};
+
+const form = document.getElementById('commit');
+
+/**
+ * @param {Event} e
+ */
+form.onsubmit = (e) => {
+  const elements = Array.from(e.srcElement).filter((e) => !!e.name);
+  const data = {};
+  elements.forEach((e) => {
+    data[e.name] = e.value;
+  });
+  console.log(data);
+  e.preventDefault();
+};
 
 export { init };
